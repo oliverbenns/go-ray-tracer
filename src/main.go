902608573f7a3ea21b35dac77face78b486406e0
Oliver Benns <oliverbenns@gmail.com>
2020-02-07 17:34:48 +0000

    Use Vec3 in initial program

--- src/main.go
+++ src/main.go
@@ -23,12 +23,15 @@ func main() {
 
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			r := float64(i) / float64(nx)
-			g := float64(j) / float64(ny)
-			b := 0.2
-			ir := int64(255.99 * r)
-			ig := int64(255.99 * g)
-			ib := int64(255.99 * b)
+			color := Vec3{
+				float64(i) / float64(nx),
+				float64(j) / float64(ny),
+				0.2,
+			}
+
+			ir := int64(255.99 * color.R())
+			ig := int64(255.99 * color.G())
+			ib := int64(255.99 * color.B())
 			data = fmt.Sprint(data, ir, " ", ig, " ", ib, "\n")
 		}
 	}
