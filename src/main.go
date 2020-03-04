34d8b9214c34f75796a20e9e2993b3ca22cc0e59
Oliver Benns <oliverbenns@gmail.com>
2020-03-04 14:02:29 +0000

    Render gradient

--- src/main.go
+++ src/main.go
@@ -26,18 +26,30 @@ func saveImage(img *image.RGBA, filename string) error {
 	return png.Encode(file, img)
 }
 
+func calculateColor(r *Ray) Color {
+	unitDirection := r.direction.UnitVec()
+	t := 0.5 * (unitDirection.y + 1.0)
+	v := Vec3{1.0, 1.0, 1.0}.MultF(1.0 - t).Add(Vec3{0.5, 0.7, 1.0}.MultF(t))
+
+	return Color{v.x, v.y, v.z}
+}
+
 func main() {
 	nx := 200
 	ny := 100
 	img := image.NewRGBA(image.Rect(0, 0, nx, ny))
 
+	lowerLeftCorner := Vec3{-2.0, -1.0, -1.0}
+	horizontal := Vec3{4.0, 0.0, 0.0}
+	vertical := Vec3{0.0, 2.0, 0.0}
+	origin := Vec3{0.0, 0.0, 0.0}
+
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			col := Color{
-				float64(i) / float64(nx),
-				float64(ny-j-1) / float64(ny),
-				0.2,
-			}.MultF(0xffff)
+			u := float64(i) / float64(nx)
+			v := float64(ny-j-1) / float64(ny)
+			r := Ray{origin, lowerLeftCorner.Add(horizontal.MultF(u)).Add(vertical.MultF(v))}
+			col := calculateColor(&r).MultF(0xffff)
 
 			imageColor := color.RGBA64{
 				uint16(col.r),
